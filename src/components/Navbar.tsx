43e176e51d7e691634f1d122930509bdab5b67c2
leoAshu <ashutosh.ojha2009@gmail.com>
2023-11-03 22:47:30 +0530

    sets up tailwindcss and designs navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,45 @@
+import { Link } from 'react-router-dom'
+import { Stack } from '@mui/material'
+
+import Logo from '../assets/images/Logo.png'
+
 const Navbar = () => {
-    return <div>Navbar</div>
+    return (
+        <Stack
+            direction="row"
+            justifyContent="space-around"
+            sx={{
+                gap: { sm: '122px', xs: '40px' },
+                mt: { sm: '32px', xs: '20px' },
+                justifyContent: 'none',
+                px: '20px',
+            }}
+        >
+            <Link to="/">
+                <img src={Logo} alt="logo" className="w-12 h-12 mx-5" />
+            </Link>
+            <Stack
+                direction="row"
+                gap="40px"
+                fontSize="24px"
+                alignItems="flex-end"
+            >
+                <Link
+                    to="/"
+                    style={{
+                        textDecoration: 'none',
+                        color: '#3a1212',
+                        borderBottom: '3px solid #FF2625',
+                    }}
+                >
+                    Home
+                </Link>
+                <a href="#exercises" className="no-underline text-[#3A1212]">
+                    Exercises
+                </a>
+            </Stack>
+        </Stack>
+    )
 }
 
 export default Navbar
