1b556459d43d1ba1388efca27921af3a70c6862b
leoAshu <ashutosh.ojha2009@gmail.com>
2023-11-08 16:06:24 +0530

    designs the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,37 @@
+import { Box, Stack, Typography } from '@mui/material'
+import Logo from '../assets/images/Logo.png'
+
 const Footer = () => {
-    return <div>Footer</div>
+    return (
+        <Box mt="80px" bgcolor="#FFF3F4">
+            <Stack
+                gap="40px"
+                sx={{ alignItems: 'center' }}
+                flexWrap="wrap"
+                px="40px"
+                pt="36px"
+                pb="36px"
+            >
+                <Stack
+                    direction="row"
+                    alignItems="center"
+                    justifyContent="center"
+                    ml="-30px"
+                    gap="8px"
+                >
+                    <img src={Logo} alt="logo" />
+                    <Typography
+                        color="#FF2625"
+                        fontWeight="600"
+                        fontSize="26px"
+                        mt="16px"
+                    >
+                        ExerCyclopedia
+                    </Typography>
+                </Stack>
+            </Stack>
+        </Box>
+    )
 }
 
 export default Footer
